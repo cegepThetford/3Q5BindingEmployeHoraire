ffecbb0423c350b3d135edd5f4fc389e61e69a03
Marco Guay <mguay@cegepthetford.ca>
2022-06-22 07:36:32 -0400

    Ajoutez des fichiers projet.

---
+++ BindingEmployeHoraire_Affaires/EmployeHoraire.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingEmployeHoraire_Affaires
+{
+    public class EmployeHoraire
+    {
+        private string _nom;
+        private decimal _tauxHoraire;
+        private decimal _nbHeures;
+
+        public EmployeHoraire(string nom, decimal taux)
+        {
+            _nom = nom;
+            _tauxHoraire = taux;
+            _nbHeures = 0.0m;
+        }
+        public void ajouterHeures(decimal nbH)
+        {
+            _nbHeures += nbH;
+        }
+        public string Nom { get { return _nom; } set { _nom = value; } }
+        public decimal TauxHoraire { get { return _tauxHoraire; } set { _tauxHoraire = value; } }
+        public decimal NbHeures { get { return _nbHeures; } set { _nbHeures = value; } }
+        public decimal calculerSalaireBrut()
+        {
+            decimal salaire = _tauxHoraire * _nbHeures;
+            if (_nbHeures > 75.0m)
+                salaire += 0.5m * _tauxHoraire * (_nbHeures - 75.0m);
+            return salaire;
+        }
+        public override int GetHashCode()
+        {
+            return _nom.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is EmployeHoraire))
+                return false;
+            EmployeHoraire empl = (EmployeHoraire)obj;
+            return empl.Nom.Equals(_nom);
+        }
+    }
+}
---
+++ BindingEmployeHoraire_UI/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using BindingEmployeHoraire_Affaires;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BindingEmployeHoraire_UI
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EmployeHoraire _employe;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _employe = new EmployeHoraire("Adam Bernard", 10.00m);
+        }
+
+        private void AjouterHeures_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            string chaine = b.Content.ToString().Split()[1];
+            _employe.ajouterHeures(int.Parse(chaine));
+        }
+    }
+}
